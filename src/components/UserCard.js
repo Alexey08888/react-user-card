c1d2210ab71b52beab61f470dec3fbc628ec8bb1
Alexey <8888.aleksiii@gmail.com>
2021-09-20 15:44:02 +0300

    Add styles

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -7,7 +7,7 @@ function UserCard() {
       <div className="name">
         <h3>John Doe</h3>{" "}
       </div>
-      <div className="block2"><p>Share high-quality media without extra fees, text on Wi-Fi, see when
+      <div className="block2"><p >Share high-quality media without extra fees, text on Wi-Fi, see when
         your friends are typing and enjoy better group chats.*</p>
         
       </div>
